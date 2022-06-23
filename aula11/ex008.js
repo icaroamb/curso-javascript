3ce135ba9b621e6c88dc47db5a0e3f8b357677fd
Icaro Mariano <icarodealmeida@live.com>
2022-06-23 11:04:48 -0300

    curso de js

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 65
+console.log(`A velocidade do seu veículo é ${vel}km/h`)
+if(vel > 65){
+    console.log('Você está acima da velocidade permitida! MULTADO!!!')
+}
+console.log('Dirija sempre usando o cinto de segurança!')
