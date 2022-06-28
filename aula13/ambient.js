9fe3146e7981f893438c3693ecebaca4226a461b
Icaro Mariano <icarodealmeida@live.com>
2022-06-28 13:05:03 -0300

    atualizei

---
+++ aula13/ambient.js
@@ -0,0 +1,16 @@
+let c = 1
+while (c <= 500){
+    console.log(`Passo ${c}`)
+    c++
+}
+
+
+
+
+
+/*console.log('Olá, Mundo!')
+console.log('Olá, Mundo!')
+console.log('Olá, Mundo!')
+console.log('Olá, Mundo!')
+console.log('Olá, Mundo!')
+*/
