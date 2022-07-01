cbcdccdc1e18df1021d79b515158db54849ead31
Icaro Mariano <icarodealmeida@live.com>
2022-07-01 12:16:29 -0300

    Update script.js

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -6,18 +6,23 @@ function calcular(){
 
     if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
         alert('[ERRO] Faltam dados Válidos!!')
+        res.innerHTML = 'Impossível contar'
     } else {
         res.innerHTML = 'Contando: '
         var i = Number(inicio.value)
         var f = Number(fim.value)
         var p = Number(passo.value)
+        if (p <= 0) {
+            alert('Passo inválido!!! Considerando PASSO = 1')
+            p = 1
+        }
         if (i < f) {
-            for(var c = i; c <= f; c += p){
+            for(var c = i; c <= f; c += p){ //CRESCENTE
             res.innerHTML += `${c} \u{1F449}`
             }
         
         } else if (i > f){
-            for(var c = i; c > f; c -= p){
+            for(var c = i; c > f; c -= p){ //DECRESCENTE
             res.innerHTML +=  `${c} \u{1F449}`   
             }
         } else {
